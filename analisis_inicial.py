3ba7bc290f49d6392a798980cdf649f364da08ad
JohnLopez <jclopezmo@unal.edu.co>
2024-10-03 17:03:53 -0500

    nueva columna

--- analisis_inicial.py
+++ analisis_inicial.py
@@ -5,26 +5,15 @@ import seaborn as sns
 import sqlite3  as sql
 def espacios ():
     print("\n"*2)
-# direccion="MiningProcess_Flotation_Plant_Database.csv"
-# data_01 = pd.read_csv(direccion)
-# for col in data_01.columns[1:]:
-#     data_01[col] = data_01[col].str.replace(',', '.').astype(float)
-# data_01['date'] = pd.to_datetime(data_01['date'])
-# data_01.set_index('date', inplace=True)
-# print(data_01.head())
-# data_01.to_csv('data_01_nuevo.csv')
+
 direccion_nuevo="data_01_nuevo.csv"
 data_01 = pd.read_csv(direccion_nuevo)
-print(data_01.head())
+# print(data_01.head())
 data_01['date'] = pd.to_datetime(data_01['date'])
 data_01.set_index('date', inplace=True)
 
-
-# conn = sql.connect('data_01_nuevo.db')
-# data_01_nuevo.to_sql('data_01_nuevo', conn)
-# conn.close()
-print(data_01.head())
-espacios()
+# print(data_01.head())
+# espacios()
 print(data_01.info())
 espacios()
 print(data_01.describe())
@@ -32,8 +21,10 @@ espacios()
 plt.figure(figsize=(12, 6))
 sns.heatmap(data_01.corr(), annot=True, cmap='coolwarm')
 plt.title('Correlación de variables')
-plt.show()
-columnas=data_01.columns[0:-2]
+# plt.show()
+data_01["Fe rise"] = data_01["% Iron Concentrate"] - data_01["% Iron Feed"]
+columnas=data_01.columns
+print(columnas)
 columnas_2=data_01.columns[-2:]
 
 # for col in columnas:
